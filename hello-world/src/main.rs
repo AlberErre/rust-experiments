eb73650b02b529205abb7ffc3a1b28f175c1ff9f
Alber Erre <arblugo@gmail.com>
2021-07-24 09:08:16 +0100

    feat: add cool_println macro

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,12 @@
+macro_rules! cool_println {
+    // This macro takes an expression of type `expr` and prints it
+    // The `expr` designator is used for expressions.
+    ($line_to_print:expr) => {
+        println!("**{}**", $line_to_print);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    cool_println!("Hello, world!");
 }
